7e1184dc693be56947c2d396d6dea4f2d48d28f2
Gervin Luide <gervin.luide@khk.ee>
2022-10-31 00:17:41 +0200

    tuule suund, kiirus, õhurõhk, nähtavus ja niiskuse lisamine

--- classes/UI.js
+++ classes/UI.js
@@ -3,6 +3,12 @@ class UI {
         this.description = document.querySelector("#description")
         this.temp = document.querySelector("#temp")
         this.city = document.querySelector("#location")
+        this.deg = document.querySelector("#deg")
+        this.speed = document.querySelector("#speed")
+        this.humidity = document.querySelector("#humidity")
+        this.pressure = document.querySelector("#pressure")
+        this.visibility = document.querySelector("#visibility")
+
     }
 
     drawWeather(data){
@@ -10,7 +16,12 @@ class UI {
         let description = data.weather[0].description;
 
         this.description.innerHTML = description;
+        this.humidity.innerHTML = data.main.humidity
+        this.pressure.innerHTML = data.main.pressure
         this.temp.innerHTML = celcius + "&deg;";
+        this.visibility.innerHTML = data.visibility
+        this.deg.innerHTML = data.wind.deg
+        this.speed.innerHTML = data.wind.speed
         this.city.innerHTML = data.name;
     }
 }
